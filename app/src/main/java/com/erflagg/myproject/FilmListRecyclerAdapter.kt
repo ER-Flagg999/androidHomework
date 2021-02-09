81ceafa786438f9e6d5ebbb645ae2eec3b9a91a7
ER-Flagg999 <ER.Flagg999@gmail.com>
2021-02-09 19:26:07 +0300

    added RV with films information and film's details activity

---
+++ app/src/main/java/com/erflagg/myproject/FilmListRecyclerAdapter.kt
@@ -0,0 +1,50 @@
+package com.erflagg.myproject
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.erflagg.myproject.databinding.FilmItemBinding
+
+//в параметр передаем слушатель, чтобы мы потом могли обрабатывать нажатия из класса Activity
+class FilmListRecyclerAdapter(private val clickListener: OnItemClickListener) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    //Здесь у нас хранится список элементов для RV
+    private val items = mutableListOf<Film>()
+
+    //Этот метод нужно переопределить на возврат количества элементов в списке RV
+    override fun getItemCount() = items.size
+
+    //В этом методе мы привязываем наш ViewHolder и передаем туда "надутую" верстку нашего фильма
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val binding = FilmItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return FilmViewHolder(binding)
+    }
+
+    //В этом методе будет привязка полей из объекта Film к View из film_item.xml
+        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+            val filmViewHolder = holder as FilmViewHolder
+        filmViewHolder.itemBinding.title.text = items[position].title
+        filmViewHolder.itemBinding.description.text = items[position].description
+        filmViewHolder.itemBinding.poster.setImageResource(items[position].poster)
+
+        holder.itemView.setOnClickListener {
+            clickListener.click(items[position])
+        }
+
+        }
+
+    //Метод для добавления объектов в наш список
+    fun addItems(list: List<Film>) {
+        //Сначала очищаем(если не реализовать DiffUtils)
+        items.clear()
+        //Добавляем
+        items.addAll(list)
+        //Уведомляем RV, что пришел новый список, и ему нужно заново все "привязывать"
+        notifyDataSetChanged()
+    }
+
+
+    //Интерфейс для обработки кликов
+    interface OnItemClickListener {
+        fun click(film: Film)
+    }
+}
---
+++ app/src/main/java/com/erflagg/myproject/FilmViewHolder.kt
@@ -0,0 +1,9 @@
+package com.erflagg.myproject
+
+import androidx.recyclerview.widget.RecyclerView
+import com.erflagg.myproject.databinding.FilmItemBinding
+
+
+class FilmViewHolder(var itemBinding: FilmItemBinding) : RecyclerView.ViewHolder(itemBinding.root) {
+
+}
